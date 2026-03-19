afc19de1501b6c3f22c1b164c3ee41bf4286dc5c
Omar Anwar <anwrmr649@gmail.com>
2026-03-19 22:45:57 +0200

    add swiping gesture to close popover carousel

--- app/components/PopoverCarousel.tsx
+++ app/components/PopoverCarousel.tsx
@@ -2,6 +2,8 @@ import { X } from "lucide-react";
 import { Button } from "./ui/button";
 import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import Image from "next/image";
+import { motion, useMotionValue, useTransform } from "motion/react";
+import { useEffect } from "react";
 
 function PopoverCarousel({
   selectedImages,
@@ -18,10 +20,23 @@ function PopoverCarousel({
   imageIndex: number;
   setClosed: () => void;
 }) {
+  const y = useMotionValue(0);
+  const yPercentage = useTransform(
+    y,
+    (value) => 1 - Math.abs(value) / (window.screen.height / 3),
+  );
+  useEffect(() => {
+    if (selectedImages.length > 0) y.set(0);
+    console.log("no");
+  }, [selectedImages, y]);
   return (
     <div
-      className={`${selectedImages.length > 0 ? "fixed" : "hidden"} bg-popover inset-0 touch-pinch-zoom lg:hidden`}
+      className={`${selectedImages.length > 0 ? "fixed" : "hidden"} inset-0 touch-pinch-zoom lg:hidden`}
     >
+      <motion.div
+        className="bg-popover fixed inset-0"
+        style={{ opacity: yPercentage }}
+      />
       <Button
         size={"icon-xs"}
         variant={"outline"}
@@ -30,27 +45,49 @@ function PopoverCarousel({
       >
         <X />
       </Button>
-      <div className="flex h-full w-full items-center justify-center">
-        <Carousel opts={{ dragFree: false, startIndex: imageIndex }}>
-          <CarouselContent>
+      <div className="flex h-full w-full justify-center">
+        <Carousel
+          opts={{ dragFree: false, startIndex: imageIndex }}
+          className="h-full"
+        >
+          <CarouselContent className="h-full">
             {selectedImages.map((image, index) => (
               <CarouselItem key={index} className="basis-full">
-                {image.width && image.height ? (
-                  <Image
-                    src={image.url}
-                    alt={image.alt}
-                    width={image.width}
-                    height={image.height}
-                    className="max-h-svh max-w-full object-contain"
-                  />
-                ) : (
-                  // eslint-disable-next-line @next/next/no-img-element
-                  <img
-                    src={image.url}
-                    alt={image.alt}
-                    className="max-h-svh max-w-full object-contain"
-                  />
-                )}
+                <motion.div
+                  className="z-2"
+                  drag="y"
+                  dragDirectionLock={true}
+                  dragConstraints={{ top: 0, bottom: 0 }}
+                  dragElastic={0.8}
+                  onDragEnd={(_, info) => {
+                    if (Math.abs(y.get()) < 1) return;
+                    if (
+                      Math.abs(info.offset.y) > window.screen.height / 3 ||
+                      info.velocity.y > 500
+                    ) {
+                      setClosed();
+                      y.set(0);
+                    }
+                  }}
+                  style={{ y }}
+                >
+                  {image.width && image.height ? (
+                    <Image
+                      src={image.url}
+                      alt={image.alt}
+                      width={image.width}
+                      height={image.height}
+                      className="max-h-svh max-w-full object-contain"
+                    />
+                  ) : (
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img
+                      src={image.url}
+                      alt={image.alt}
+                      className="max-h-svh max-w-full object-contain"
+                    />
+                  )}
+                </motion.div>
               </CarouselItem>
             ))}
           </CarouselContent>
