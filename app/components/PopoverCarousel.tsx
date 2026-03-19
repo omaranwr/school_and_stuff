560bbe3b2fa3bb4e51ec37378ed9c0d48ecd9e86
Omar Anwar <anwrmr649@gmail.com>
2026-03-19 22:58:25 +0200

    fixing zooming issue

--- app/components/PopoverCarousel.tsx
+++ app/components/PopoverCarousel.tsx
@@ -31,7 +31,7 @@ function PopoverCarousel({
   }, [selectedImages, y]);
   return (
     <div
-      className={`${selectedImages.length > 0 ? "fixed" : "hidden"} inset-0 touch-pinch-zoom lg:hidden`}
+      className={`${selectedImages.length > 0 ? "fixed" : "hidden"} inset-0 lg:hidden`}
     >
       <motion.div
         className="bg-popover fixed inset-0"
