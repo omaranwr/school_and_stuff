b7f70c3ca0691f14d38ae9a64876094c62931568
Omar Anwar <anwrmr649@gmail.com>
2026-03-20 04:25:55 +0200

    prevent swiping down when dragging carousel

--- app/components/PopoverCarousel.tsx
+++ app/components/PopoverCarousel.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import { X } from "lucide-react";
 import { Button } from "./ui/button";
-import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
+import {
+  Carousel,
+  CarouselApi,
+  CarouselContent,
+  CarouselItem,
+} from "./ui/carousel";
 import Image from "next/image";
-import { motion, useMotionValue, useTransform } from "motion/react";
-import { useEffect } from "react";
+import {
+  motion,
+  MotionValue,
+  useMotionValue,
+  useTransform,
+} from "motion/react";
+import { useEffect, useState } from "react";
+
+const screenHeight = typeof window === undefined ? window.screen.height : 1;
 
 function PopoverCarousel({
   selectedImages,
@@ -20,15 +34,40 @@ function PopoverCarousel({
   imageIndex: number;
   setClosed: () => void;
 }) {
+  const [api, setApi] = useState<CarouselApi>();
+  const [isTransitioning, setIsTransitioning] = useState<boolean>(false);
+
   const y = useMotionValue(0);
   const yPercentage = useTransform(
     y,
-    (value) => 1 - Math.abs(value) / (window.screen.height / 3),
+    (value) => 1 - Math.abs(value) / (screenHeight / 3),
   );
+
+  useEffect(() => {
+    api?.on("select", () => setIsTransitioning(true));
+    api?.on("settle", () => setIsTransitioning(false));
+    api?.on("scroll", (emblaApi) => {
+      const settlePixelThreshold = 50;
+      const { dragHandler, location, target } = emblaApi.internalEngine();
+      if (dragHandler.pointerDown()) {
+        setIsTransitioning(true);
+        return;
+      }
+
+      const displacement = target.get() - location.get();
+      if (Math.abs(displacement) < settlePixelThreshold) {
+        setIsTransitioning(false);
+        return;
+      }
+
+      setIsTransitioning(true);
+    });
+  }, [api]);
+
   useEffect(() => {
     if (selectedImages.length > 0) y.set(0);
-    console.log("no");
   }, [selectedImages, y]);
+
   return (
     <div
       className={`${selectedImages.length > 0 ? "fixed" : "hidden"} inset-0 touch-none lg:hidden`}
@@ -49,51 +88,77 @@ function PopoverCarousel({
         <Carousel
           opts={{ dragFree: false, startIndex: imageIndex }}
           className="h-full"
+          setApi={setApi}
         >
           <CarouselContent className="h-full">
             {selectedImages.map((image, index) => (
               <CarouselItem key={index} className="basis-full">
-                <motion.div
-                  className="z-2"
-                  drag="y"
-                  dragDirectionLock={true}
-                  dragConstraints={{ top: 0, bottom: 0 }}
-                  dragElastic={0.8}
-                  onDragEnd={(_, info) => {
-                    if (Math.abs(y.get()) < 1) return;
-                    if (
-                      Math.abs(info.offset.y) > window.screen.height / 3 ||
-                      info.velocity.y > 500
-                    ) {
-                      setClosed();
-                      y.set(0);
-                    }
-                  }}
-                  style={{ y }}
-                >
-                  {image.width && image.height ? (
-                    <Image
-                      src={image.url}
-                      alt={image.alt}
-                      width={image.width}
-                      height={image.height}
-                      className="max-h-svh max-w-full object-contain"
-                    />
-                  ) : (
-                    // eslint-disable-next-line @next/next/no-img-element
-                    <img
-                      src={image.url}
-                      alt={image.alt}
-                      className="max-h-svh max-w-full object-contain"
-                    />
-                  )}
-                </motion.div>
+                <PopoverCarouselItem
+                  image={image}
+                  y={y}
+                  setClosed={setClosed}
+                  isScrolling={isTransitioning}
+                />
               </CarouselItem>
             ))}
           </CarouselContent>
         </Carousel>
       </div>
     </div>
+  );
+}
+
+function PopoverCarouselItem({
+  image,
+  y,
+  setClosed,
+  isScrolling,
+}: {
+  image: {
+    alt: string;
+    postId: number;
+    url: string;
+    width: number | null;
+    height: number | null;
+  };
+  y: MotionValue<number>;
+  setClosed: () => void;
+  isScrolling: boolean;
+}) {
+  return (
+    <motion.div
+      className="z-2"
+      drag="y"
+      dragListener={!isScrolling}
+      dragDirectionLock={true}
+      dragConstraints={{ top: 0, bottom: 0 }}
+      dragElastic={0.8}
+      onDragEnd={(_, info) => {
+        if (Math.abs(y.get()) < 1) return;
+        if (Math.abs(info.offset.y) > screenHeight || info.velocity.y > 500) {
+          setClosed();
+          y.set(0);
+        }
+      }}
+      style={{ y }}
+    >
+      {image.width && image.height ? (
+        <Image
+          src={image.url}
+          alt={image.alt}
+          width={image.width}
+          height={image.height}
+          className="max-h-svh max-w-full object-contain"
+        />
+      ) : (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={image.url}
+          alt={image.alt}
+          className="max-h-svh max-w-full object-contain"
+        />
+      )}
+    </motion.div>
   );
 }
 
