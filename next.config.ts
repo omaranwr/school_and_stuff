c2ea5bc01b911c76cf31cc0a4e780077ff683a34
Omar Anwar <anwrmr649@gmail.com>
2026-03-28 17:25:15 +0200

    increase cache life of images

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL("https://0p4uz5pl9f.ufs.sh/f/**")],
+    minimumCacheTTL: 60 * 60 * 24 * 30,
   },
 };
 
