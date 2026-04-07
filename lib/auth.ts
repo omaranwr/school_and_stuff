a1146c9d84dd51c8b3f7c514b19cb769dfa18ac4
Omar Anwar <anwrmr649@gmail.com>
2026-04-08 00:30:45 +0200

    setup betterauth infra

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,12 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { drizzle } from "drizzle-orm/better-sqlite3";
+import { db } from "@/db";
 import * as schema from "./auth-schema";
-import Database from "better-sqlite3";
-
-const db = drizzle(new Database("database.sqlite"), { schema });
+import { dash } from "@better-auth/infra";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, { provider: "sqlite", schema }),
   baseURL: process.env.BASR_URL || "http://localhost:3000/",
   emailAndPassword: { enabled: true },
+  plugins: [dash()],
 });
