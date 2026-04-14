ebc58df18b7e522e004dca71fd039af5cfdd55d2
Omar Anwar <anwrmr649@gmail.com>
2026-04-14 22:01:16 +0200

    make uploads log errors

--- components/AdminForm.tsx
+++ components/AdminForm.tsx
@@ -56,6 +56,7 @@ function AdminForm({
       onUploadProgress: (progress) => {
         setProgress(progress);
       },
+      onUploadError: (e) => console.log(e),
     },
   );
   const { getRootProps, getInputProps } = useDropzone({
